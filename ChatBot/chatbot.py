41bdeee2bca7298dac49aa394b25933f8b8fabd3
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-10-01 00:08:57 +0300

    ChatBot 2-st

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,2 +1,11 @@
 print("Hello! My name is Alex")
 print("I was created in 2021")
+while True:
+    a = input("")
+    g = 'hello' and 'hi'
+    if a != g:
+        print('And say hello or hi?!')
+    if 'hello' and 'hi' in a:
+        b = input("Please, remind me your name: ")
+        print('What a great name you have, ', b, '!')
+    continue
