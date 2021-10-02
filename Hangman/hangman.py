7cfb7f246dafe98259e103c162784b02005fb86f
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-10-02 21:01:07 +0300

    Hangman 2-st

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,1 +1,8 @@
 print("HANGMAN"  "\nThe game will be available soon.")
+word = 'python'
+print('Guess the word:')
+a = input()
+if a == word:
+    print("You survived!")
+else:
+    print("You lost!")
