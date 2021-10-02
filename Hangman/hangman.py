5cb0db6d4150f106ad2b83c9aacdf4f053fefc2f
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-10-02 21:11:20 +0300

    Hangman 3-st

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,5 +1,8 @@
+import random
 print("HANGMAN"  "\nThe game will be available soon.")
-word = 'python'
+the_words = ['python', 'java', 'javascript', 'php']
+word = the_words[random.randrange(4)]
+#print('answer', word)
 print('Guess the word:')
 a = input()
 if a == word:
