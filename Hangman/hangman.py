edf200b69674b46fee703cb2b2104b96bd1b15fb
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-10-11 17:06:45 +0300

    Hangman 5-st

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -6,12 +6,46 @@ exit()
 повторы 
 не прав + повтор+несколько букв+ не символ а цифра 
 """
+
 print("HANGMAN"  "\nThe game will be available soon.")
-the_words = ['python', 'java', 'javascript', 'php']
-word = random.choice(the_words)
-print(word[0:len(word)-3]+"---")
-inputChar = input()
-if inputChar in word:
-    print("You survived!")
-else:
-    print("You lost!")
+words = ['python', 'bask', "java", "javascript", "php"]
+the_word = random.choice(words)
+print("The word has", len(the_word), 'letters')
+right = ['-'] * len(the_word)
+wrong = []
+health = 8
+
+
+def update():
+    for i in right:
+        print(i, end='')
+    print()
+update()
+
+
+while True:
+    print('health', health)
+    conjec = input("conjecture a lettre: ")
+    health -= 1
+    if conjec in the_word:
+        index = 0
+        for i in the_word:
+            if i == conjec:
+                right[index] = conjec
+            index += 1
+        update()
+    else:
+        if conjec not in wrong:
+            wrong.append(conjec)
+        else:
+            print("You already conjecture that")
+        print(wrong)
+        if health == 0:
+            print("You lost!")
+            break
+        #if len(wrong) > health:
+        continue
+    if '-' not in right:
+        print("You survived!")
+        print('I conject', the_word)
+        break
