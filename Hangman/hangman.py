d5e5a48f7ca087b3f8111e31e527520911e3451f
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-10-11 17:43:20 +0300

    Hangman 6-st

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -14,7 +14,8 @@ print("The word has", len(the_word), 'letters')
 right = ['-'] * len(the_word)
 wrong = []
 health = 8
-
+len_word = len(the_word)
+used_letters = ""
 
 def update():
     for i in right:
@@ -26,26 +27,32 @@ update()
 while True:
     print('health', health)
     conjec = input("conjecture a lettre: ")
-    health -= 1
-    if conjec in the_word:
-        index = 0
-        for i in the_word:
-            if i == conjec:
-                right[index] = conjec
-            index += 1
-        update()
+    if conjec in used_letters or len(conjec) > 1:
+        print("No improvements")
+        health -= 1
     else:
-        if conjec not in wrong:
-            wrong.append(conjec)
+        used_letters += conjec
+
+        if conjec in the_word:
+            index = 0
+            for i in the_word:
+                if i == conjec:
+                    right[index] = conjec
+                index += 1
+            update()
         else:
-            print("You already conjecture that")
-        print(wrong)
-        if health == 0:
-            print("You lost!")
+            if conjec not in wrong:
+                wrong.append(conjec)
+            else:
+                print(wrong)
+            health -= 1
+            print("That letter doesn't appear in the word")
+            if health == 0:
+                print("You lost!")
+                break
+            #if len(wrong) > health:
+            continue
+        if '-' not in right:
+            print("You survived!")
+            print('I conject', the_word)
             break
-        #if len(wrong) > health:
-        continue
-    if '-' not in right:
-        print("You survived!")
-        print('I conject', the_word)
-        break
